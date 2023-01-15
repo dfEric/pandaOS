c101c6cf2a65fe51d2dd8f026890f159568542b4
Zhengxl1987 <122581230+Zhengxl1987@users.noreply.github.com>
2023-01-15 20:32:14 +0800

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,22 +1,23 @@
 ; filename boot.asm
-
-MBOOT_MAGIC equ 0x1BADB002
-MBOOT_FLAGS equ 0x00
-MBOOT_CHECKSUM equ - (MBOOT_MAGIC + MBOOT_FLAGS)
-
-[BITS 32]
-
+; Copyright: www.cnblogs.com/lucasysfeng
+ 
+MBOOT_MAGIC  equ 0x1BADB002  ; multiboot magic域，必须为此值
+MBOOT_FLAGS  equ 0x00        ; multiboot flag域, GRUB启动时是否要做一些特殊操作
+MBOOT_CHECKSUM  equ -(MBOOT_MAGIC + MBOOT_FLAGS) ; multiboot checksum域，校验上面两个域是否正确
+ 
+[BITS 32]                    ; 以32位编译
+ 
 section .text
-	dd MBOOT_MAGIC
-	dd MBOOT_FLAGS
-	dd MBOOT_CHECKSUM
-	dd start
-
+   dd  MBOOT_MAGIC
+   dd  MBOOT_FLAGS
+   dd  MBOOT_CHECKSUM
+   dd  start
+ 
 [GLOBAL start]
-[EXTERN kernel_main]
-
+[EXTERN kernel_main]         ; 内核入口函数, EXTERN表明此符号在外部定义
+ 
 start:
-	cli
-	call kernel_main
-	jmp $
+   cli                        ; 禁用中断
+   call kernel_main           ; 调用内核入口函数
+   jmp $
 
